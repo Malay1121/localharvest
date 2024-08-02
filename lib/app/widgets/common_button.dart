5676e0d42b2bfb25d5d4de639b35c057f7265705
Malay1121 <dhara9277@gmail.com>
2024-08-02 22:54:55 +0530

    Added common widgets, linked with Firebase

---
+++ lib/app/widgets/common_button.dart
@@ -0,0 +1,52 @@
+import 'package:local_harvest/app/helper/all_imports.dart';
+
+import 'common_text.dart';
+
+class CommonButton extends StatefulWidget {
+  CommonButton(
+      {super.key,
+      required this.text,
+      required this.onTap,
+      this.height = 48,
+      this.width = 342,
+      this.backgroundColor,
+      this.textColor});
+  String text;
+  VoidCallback onTap;
+  double height;
+  double width;
+  Color? backgroundColor;
+  Color? textColor;
+  @override
+  State<CommonButton> createState() => _CommonButtonState();
+}
+
+class _CommonButtonState extends State<CommonButton> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.onTap,
+      child: Container(
+        height: widget.height.h(context),
+        width: widget.width.w(context),
+        decoration: BoxDecoration(
+          color: widget.backgroundColor ?? AppColors.primary,
+          borderRadius: BorderRadius.circular(61),
+        ),
+        child: Center(
+          child: AppText(
+            text: widget.text,
+            height: 24.h(context),
+            centered: true,
+            style: TextStyle(
+              color: widget.textColor ?? AppColors.white,
+              fontFamily: dmSans,
+              fontWeight: FontWeight.w500,
+              fontSize: 16.t(context),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/widgets/common_textfield.dart
@@ -0,0 +1,140 @@
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
+
+import '../helper/all_imports.dart';
+
+class CommonTextField extends StatefulWidget {
+  CommonTextField({
+    super.key,
+    required this.hintText,
+    this.height = 45,
+    this.width = 342,
+    this.prefixIcon,
+    this.controller,
+    this.keyboardType,
+    this.obscureText = false,
+    this.textInputAction,
+    this.maxLines = 1,
+  });
+  String hintText;
+  double? width;
+  double? height;
+  Widget? prefixIcon;
+  TextEditingController? controller;
+  bool obscureText;
+  TextInputType? keyboardType;
+  TextInputAction? textInputAction;
+  int? maxLines;
+
+  @override
+  State<CommonTextField> createState() => _CommonTextFieldState();
+}
+
+class _CommonTextFieldState extends State<CommonTextField> {
+  bool listening = false;
+  SpeechToText speechToText = SpeechToText();
+  bool speechEnabled = false;
+
+  void speak() {
+    if (!listening) {
+      getText();
+    }
+  }
+
+  void initializeSpeech() async {
+    speechEnabled = await speechToText.initialize(
+      onError: (errorNotification) {
+        setState(() {
+          listening = false;
+        });
+      },
+    );
+  }
+
+  Future<String> getText() async {
+    String result = "";
+    setState(() {
+      listening = true;
+    });
+
+    SpeechRecognitionResult r = await speechToText.listen(
+        listenOptions: SpeechListenOptions(listenMode: ListenMode.dictation),
+        partialResults: false,
+        onResult: (res) {
+          setState(() {
+            result = res.recognizedWords;
+            if (widget.controller != null) widget.controller!.text += result;
+            listening = false;
+          });
+        });
+
+    return result;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // TODO: implement initState
+    initializeSpeech();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: widget.width?.w(context),
+      height: widget.height?.h(context),
+      child: TextField(
+        controller: widget.controller,
+        obscureText: widget.obscureText,
+        keyboardType: widget.keyboardType,
+        maxLines: widget.maxLines,
+        textInputAction: widget.textInputAction,
+        decoration: InputDecoration(
+          fillColor: AppColors.white,
+          filled: true,
+          hintText: widget.hintText,
+          suffixIcon: GestureDetector(
+            onTap: () => speak(),
+            child: Icon(
+              listening ? Icons.stop : Icons.mic,
+              color: AppColors.primary,
+              size: 24.t(context),
+            ),
+          ),
+          hintStyle: Styles.medium(
+            color: AppColors.fontDark,
+          ),
+          prefixIcon: widget.prefixIcon,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: AppColors.stroke,
+              width: 1,
+            ),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: AppColors.stroke,
+              width: 1,
+            ),
+          ),
+          disabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: AppColors.stroke,
+              width: 1,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8),
+            borderSide: BorderSide(
+              color: AppColors.stroke,
+              width: 1,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
